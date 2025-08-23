e1e81e36aead5ccc3a37ce3718133d51dc50d6b9
nexx14 <mnabilcf65@gmail.com>
2025-08-23 22:19:29 +0700

    add icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,11 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'M Nabil CF | Junior Fullsack Developer Portofolio',
+  icons: {
+    icon: '/icon.png',
+    shortcut: '/icon.png',
+    apple: '/icon.png',
+  },
 }
 
 export default function RootLayout({
